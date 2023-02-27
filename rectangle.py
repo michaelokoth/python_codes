4f01aff36f5fb2bb166099de861781648b65e7fb
michaelokoth <114147018+michaelokoth@users.noreply.github.com>
2023-02-27 15:25:05 +0300

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,12 @@
+#class rectangle
+class rectangle:
+    def __init__(self,length,width):
+        self.length=length      
+        self.width=width
+    def area(self):
+        return self.length*self.width
+    def perimeter(self):
+        return 2*(self.length+self.width)
+r=rectangle(5,8)
+print(r.area())
+print(r.perimeter())
